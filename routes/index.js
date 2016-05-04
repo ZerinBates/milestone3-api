c512d08fe578265fd21afe5132939c703f4d00fb
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-05-04 12:21:44 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send([
-    { id: 1, slug: 'How to pass class', body: "Bring girl scout samoas Cookies!" },
-    { id: 2, slug: 'How to fail class', body: "Dont do anything!" }
-  ]);
+  res.render('index', { title: 'Today I Learned' });
 });
 
 module.exports = router;
