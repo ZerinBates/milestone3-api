86751c51bf066ab6f4a3c051435a9d604b5209a2
zerinTheGreatAndTerrible <zerinbates@gmail.com>
2016-05-06 14:05:54 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/til/create', function(req, res, next) {
 });
 router.get('/delete/:id', function(req, res, next) {
   console.log(req.query);
-  entries.[req.params.id].slug=null;
+  entries.[req.params.id];
   console.log(entries);
   res.render('todayILearned', { title: 'Today I Learned', entries:entries});
 });
